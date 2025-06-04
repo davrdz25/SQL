d9c8ffd9237206228e173ed598b3505ad68c8a27
David Rodriguez <davrdz25@yahoo.com.mx>
2025-06-04 09:41:22 -0600

    Remove useless class

--- GraphQL/Adapters/ItemWrapper.cpp
+++
@@ -1,8 +0,0 @@
-#include "ItemWrapper.hpp"
-
-std::shared_ptr<graphql::item::object::Item> wrapItemModel(const ItemModel& model)
-{
-    return std::make_shared<graphql::item::object::Item>(
-        std::make_shared<ItemResolver>(model)
-    );
-}
--- GraphQL/Adapters/ItemWrapper.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../Generated/Item/ItemObject.h"
-#include "../Resolvers/ItemResolver.hpp"
-#include <memory>
-#include "../../Models/ItemModel.hpp"
-
-std::shared_ptr<graphql::item::object::Item> wrapItemModel(const ItemModel& model);
